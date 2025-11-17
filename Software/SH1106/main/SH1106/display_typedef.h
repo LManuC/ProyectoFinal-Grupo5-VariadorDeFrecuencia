8d6f4bfee3986018a35d26abfd8f7e3a7639e7e3
Lucas Manuel Carra <lmanuelcarra@gmail.com>
2025-11-17 07:59:20 -0300

    Borrado de proyectos viejos y deprecados

--- Software/SH1106/main/SH1106/display_typedef.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_TYPEDEF_H
-#define DISPLAY_TYPEDEF_H
-
-#include "../LVFV_system.h"
-
-#endif
--- Software/SH1106/main/SH1106/sh1106_i2c.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SH1106_I2C_H__
-
-#define __SH1106_I2C_H__
-
-esp_err_t sh1106_write_command(uint8_t cmd);
-esp_err_t sh1106_write_data(const uint8_t *data, size_t len);
-
-#endif
