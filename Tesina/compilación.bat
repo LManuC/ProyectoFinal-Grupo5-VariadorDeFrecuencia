c94cc2833f8f2e1a8f9a8967af7fd638c30cfe55
Lucas Manuel Carra <lmanuelcarra@gmail.com>
2025-12-13 11:14:03 -0300

    .

--- Tesina/compilación.bat
+++ Tesina/compilación.bat
@@ -1,13 +1,17 @@
-set "MAIN=main"
-set "OUTDIR=out"
-set "DESTPDF=Tesina_Andrenacci-Carra.pdf"
+REM El comando REM crea un comentario
+REM El comando set crea 'defines'
+REM Como ejecuto el bash desde el VSCode, en la carpeta del repo, es decir Tesina/.., primero cambio el directorio a ./Tesina, luego creo el PDF desde Latex
 
-rmdir -r "%OUTDIR%"
-mkdir "%OUTDIR%"
+set MAIN=main
+set OUTDIR=out
+set DESTPDF=Tesina_Andrenacci-Carra.pdf
 
-pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory="%OUTDIR%" "%MAIN%".tex
-biber --input-directory "%OUTDIR%" --output-directory "%OUTDIR%" "%MAIN%"
-pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory="%OUTDIR%" "%MAIN%".tex
-pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory="%OUTDIR%" "%MAIN%".tex
+cd .\Tesina
+REM rmdir -r %OUTDIR%
 
-move "%OUTDIR%"\"%MAIN%".pdf .\"%DESTPDF%"
+pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=%OUTDIR% %MAIN%.tex
+biber --input-directory %OUTDIR% --output-directory %OUTDIR% %MAIN%
+pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=%OUTDIR% %MAIN%.tex
+pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=%OUTDIR% %MAIN%.tex
+
+move %OUTDIR%\%MAIN%.pdf .\%DESTPDF%
