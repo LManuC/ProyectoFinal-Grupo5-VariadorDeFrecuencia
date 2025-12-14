869adc8600f8b472a64566d8de76aae6a1e74a10
Lucas Manuel Carra <lmanuelcarra@gmail.com>
2025-12-14 12:36:59 -0300

    Cambio de las rutas específicas a relativas en el Doxyfile y correción en doxument.bat

--- Software/LVFV_ESP32/doxument.bat
+++ Software/LVFV_ESP32/doxument.bat
@@ -4,6 +4,8 @@ set OUTDIR=out
 set DESTPDF=Documentacion-ESP32_Andrenacci-Carra.pdf
 
 doxygen doxy/Doxyfile
-call doxy/latex/make.bat
+call "doxy/latex/make.bat"
 
 move doxy\latex\refman.pdf .\%DESTPDF%
+
+pause
