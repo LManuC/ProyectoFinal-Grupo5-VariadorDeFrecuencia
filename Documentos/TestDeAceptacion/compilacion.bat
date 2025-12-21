5b2e81497161a3cf603216047e7fa7eebdecd704
Lucas Manuel Carra <lmanuelcarra@gmail.com>
2025-12-21 10:52:54 -0300

    Subida del test de aceptación

---
+++ Documentos/TestDeAceptacion/compilacion.bat
@@ -0,0 +1,15 @@
+REM El comando REM crea un comentario
+REM El comando set crea 'defines'
+REM Como ejecuto el bash desde el VSCode, en la carpeta del repo, es decir Tesina/.., primero cambio el directorio a ./Tesina, luego creo el PDF desde Latex
+
+set MAIN=main
+set OUTDIR=out
+set DESTPDF=Test-de-aceptacion_Andrenacci-Carra.pdf
+
+cd .\Documentos\TestDeAceptacion
+
+pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=%OUTDIR% %MAIN%.tex
+pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=%OUTDIR% %MAIN%.tex
+pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=%OUTDIR% %MAIN%.tex
+
+move %OUTDIR%\%MAIN%.pdf .\%DESTPDF%
